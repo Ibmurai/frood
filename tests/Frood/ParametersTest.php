7b4ae3de718a779a294730a7ed68a25fd7efdaa8
web <web@72642f53-b532-4fd9-9cad-1ceaffd491b0>
2011-06-10 09:10:23 +0000

    W.I.P. @jers

---
+++ tests/Frood/ParametersTest.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Test the parameters class for The Frood.
+ *
+ * PHP version 5
+ *
+ * @category Test
+ * @package  Frood
+ * @author   Jens Riisom Schultz <jers@fynskemedier.dk>
+ * @since    2011-06-09
+ */
+
+/**
+ * FroodParameters - Testing it!
+ *
+ * @category   Test
+ * @package    Frood
+ * @subpackage Class
+ * @author     Jens Riisom Schultz <jers@fynskemedier.dk>
+ */
+class FroodParametersTest extends PHPUnit_Framework_TestCase {
+	protected function setUp() {
+	}
+
+	/**
+	 * Tears down the fixture, for example, closes a network connection.
+	 * This method is called after a test is executed.
+	 *
+	 * @return void
+	 */
+	protected function tearDown() {
+		
+	}
+	/**
+	 * Test getTitle
+	 *
+	 * @return void
+	 */
+	public function testGetTitle() {
+		
+		$this->assertEquals('This is a document title', 'This is a document title');
+	}
+
+}
