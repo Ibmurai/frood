2e40e0cba0d1df0d0be6a99f3a8768017c51e9da
web <web@72642f53-b532-4fd9-9cad-1ceaffd491b0>
2011-06-16 10:37:47 +0000

    @jers

---
+++ src/Frood/Controller.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * An abstract controller class to use as a base for Frood controllers.
+ *
+ * PHP version 5
+ *
+ * @category Library
+ * @package  Frood
+ * @author   Jens Riisom Schultz <jers@fynskemedier.dk>
+ * @since    2011-06-16
+ */
+
+/**
+ * FroodController - An abstract controller class to use as a base for Frood controllers.
+ *
+ * @category   Library
+ * @package    Frood
+ * @subpackage Class
+ * @author     Jens Riisom Schultz <jers@fynskemedier.dk>
+ */
+abstract class FroodController {
+}
