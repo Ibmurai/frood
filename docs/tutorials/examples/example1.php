a4f8e9d9408a9ca731ea3b3bf129641fcd86ad7e
web <web@72642f53-b532-4fd9-9cad-1ceaffd491b0>
2011-06-21 08:54:02 +0000

    tweaked build script and add basic doc @jsf

---
+++ docs/tutorials/examples/example1.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * The Frood example file 1
+ *
+ * PHP Version 5
+ *
+ * @category Library
+ * @package  Frood
+ * @author   Johannes Frandsen <jsf@fynskemedier.dk>
+ * @since    2011-06-20
+ */
+echo 'Hello world';
