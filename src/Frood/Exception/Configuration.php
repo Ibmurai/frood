fceef0547fb1d4ef673061a5d66f3e6f4e6d3226
Bo Thinggaard <bo@tnactas.dk>
2011-12-16 15:38:39 +0100

    Ground work for frood base and module configuration

---
+++ src/Frood/Exception/Configuration.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * A custom Exception for configuration exceptions.
+ *
+ * PHP version 5
+ *
+ * @category Module
+ * @package  Frood
+ * @author   Jens Riisom Schultz <jers@fynskemedier.dk>
+ * @author   Bo Thinggaard <both@fynskemedier.dk>
+ * @since    2011-06-14
+ */
+
+/**
+ * FroodExceptionConfiguration - A custom Exception for configuration exceptions.
+ *
+ * @category   Module
+ * @package    Frood
+ * @subpackage Exception
+ * @author     Jens Riisom Schultz <jers@fynskemedier.dk>
+ * @author     Bo Thinggaard <both@fynskemedier.dk>
+ */
+class FroodExceptionConfiguration extends FroodException {
+}
