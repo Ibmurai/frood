0b85252e0729fbf4d1148bdb609b1fe484b16a01
Jens Riisom Schultz <jens@unpossiblesystems.dk>
2013-02-21 13:21:27 +0100

    Fixed non-PHP5.2 compliant code :(

--- src/Frood/Util.php
+++ src/Frood/Util.php
@@ -25,13 +25,7 @@ abstract class FroodUtil {
 		// First lowercase the first letter.
 		$name = strtolower(substr($name, 0, 1)) . substr($name, 1);
 
-		// Second replace capital letters with _ followed by the letter, lowercased.
-		if (PHP_MAJOR_VERSION >= 5 && PHP_MINOR_VERSION >= 3) {
-			// /e modifier is deprecated in 5.5 and the lambda function is the more elegang approach, so do that if 5.3 or better.
-			return preg_replace_callback('/([A-Z])/', function($matches) { return '_' . strtolower($matches[1]); }, $name);
-		} else {
-			return preg_replace('/([A-Z])/e', "'_'.strtolower('\\1')", $name);
-		}
+		return preg_replace_callback('/([A-Z])/', array(__CLASS__, 'convertPhpNameToHtmlNameHelper'), $name);
 	}
 
 	/**
@@ -48,13 +42,7 @@ abstract class FroodUtil {
 			$name = strtoupper(substr($name, 0, 1)) . substr($name, 1);
 		}
 
-		// Second replace _ followed by a letter with capital letters.
-		if (PHP_MAJOR_VERSION >= 5 && PHP_MINOR_VERSION >= 3) {
-			// /e modifier is deprecated in 5.5 and the lambda function is the more elegang approach, so do that if 5.3 or better.
-			return preg_replace_callback('/(_[a-z0-9])/', function($matches) { return substr(strtoupper($matches[1]),1); }, $name);
-		} else {
-			return preg_replace('/(_[a-z0-9])/e', "substr(strtoupper('\\1'),1)", $name);
-		}
+		return preg_replace_callback('/(_[a-z0-9])/', array(__CLASS__, 'convertHtmlNameToPhpNameHelper') , $name);
 	}
 
 	/**
@@ -67,5 +55,27 @@ abstract class FroodUtil {
 	 */
 	public static function cmplen($a, $b) {
 		return strlen($b) - strlen($a);
+	}
+
+	/**
+	 * Damn U PHP5.2!
+	 *
+	 * @param array $matches :(
+	 *
+	 * @return string
+	 */
+	public static function convertHtmlNameToPhpNameHelper($matches) {
+		return substr(strtoupper($matches[1]),1);
+	}
+
+	/**
+	 * Damn U PHP5.2!
+	 *
+	 * @param array $matches :(
+	 *
+	 * @return string
+	 */
+	public static function convertPhpNameToHtmlNameHelper($matches) {
+		return '_' . strtolower($matches[1]);
 	}
 }
