329b00f2230b2f897aea107e5b5064d9149718c1
PhlegmaticGuy <thomas@meganordahl.dk>
2013-10-17 15:41:08 +0200

    Update Renderer.php
    Fixed doc block.

--- src/Frood/Renderer.php
+++ src/Frood/Renderer.php
@@ -24,7 +24,7 @@ abstract class FroodRenderer {
 	/**
 	 * The constructor.
 	 *
-	 * @param string $request
+	 * @param FroodRequest $request
 	 */
 	public function __construct(FroodRequest $request) {
 		$this->_request = $request;
